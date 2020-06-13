5509b03e0de2ac64a66bbacd4b1b0797b786a029
macondo <jl.durand95@gmail.com>
2020-06-12 20:26:39 -0400

    2ndModify

--- app/users/routes.js
+++ app/users/routes.js
@@ -2,18 +2,17 @@
 const express   = require('express');
 const router    = express.Router();
 // imports & cons
-const control   = require('./responses');
+const { control }   = require('./responses');
 const { userSchemaCreate, userSchemaUpdate } = require("./schemas");
 const validatorHandler = require('../../utils/middlewares/validatorHandler');
-require('../../utils/strategies/jwt');
 
 // developer
 router.get('/', control.searchUsers());
 router.get('/:id', control.searchUserById());
 // admin
-router.post('/', validatorHandler(userSchemaCreate, 'body'), /* control.localAuth, */ control.createUser());
+router.post('/', validatorHandler(userSchemaCreate, 'body'), control.localAuth, control.createUser());
 router.put('/:id', validatorHandler(userSchemaUpdate, 'body'), control.localAuth, control.updateUserById());
-router.delete('/:id'/* , control.localAuth */, control.deleteUserById());
+router.delete('/:id', control.localAuth, control.deleteUserById());
 module.exports = router;
 
 /**
